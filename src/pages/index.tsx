9a797887b8b556a7fd0b07b53bda326b1fdf4d8f
Luisotee <luisotaviomgr1@gmail.com>
2023-04-10 05:23:54 -0300

    Teacher card

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,39 @@
-import Head from "next/head";
-import Image from "next/image";
-import { Inter } from "next/font/google";
-import styles from "@/styles/Home.module.css";
 import { DarkTheme } from "@/components/darktheme";
-import { HeaderMegaMenu } from "@/components/top-bar";
 import { LoginPage } from "@/components/login-page";
-
-const inter = Inter({ subsets: ["latin"] });
+import { TeacherCard } from "@/components/teacher-card";
+import { TopBar } from "@/components/top-bar";
+import { Center, Group } from "@mantine/core";
 
 export default function Home() {
   return (
     <>
-      <LoginPage />
+      <DarkTheme />
+      <Group spacing={50} position="center" p="xl">
+        <TeacherCard
+          avatar="https://avatars.githubusercontent.com/u/50471205?v=4"
+          name="Luis"
+          university="Facens"
+          subject="Matematica"
+        />
+        <TeacherCard
+          avatar="https://avatars.githubusercontent.com/u/50471205?v=4"
+          name="Luis"
+          university="Facens"
+          subject="Matematica"
+        />
+        <TeacherCard
+          avatar="https://avatars.githubusercontent.com/u/50471205?v=4"
+          name="Luis"
+          university="Facens"
+          subject="Matematica"
+        />
+        <TeacherCard
+          avatar="https://avatars.githubusercontent.com/u/50471205?v=4"
+          name="Luis"
+          university="Facens"
+          subject="Matematica"
+        />
+      </Group>
     </>
   );
 }
