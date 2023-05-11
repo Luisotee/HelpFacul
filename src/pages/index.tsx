f2bd61e4561e28e4731e95506b79a4a7218ee497
Luis Otavio <luisotaviomgr1@gmail.com>
2023-05-11 18:36:49 -0300

    Function to get all users from users collection in firebase

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,48 +1,37 @@
-import { TeacherCard } from "@/components/teacher-card";
+import { useState, useEffect } from "react";
 import { TopBar } from "@/components/topbar/top-bar";
 import { Group } from "@mantine/core";
-import { useRouter } from "next/router";
+import { getDataFromAllUserProfile } from "@/controller/firestore";
+import { TeacherCard } from "@/components/teacher-card";
+import { User } from "@/types";
 
 export default function Home() {
-  const router = useRouter();
-  const { user } = router.query;
+  const [users, setUsers] = useState<User[]>([]);
 
-  //console.log(user); // should output the stringified user object
+  useEffect(() => {
+    const fetchUsers = async () => {
+      const snapshot = await getDataFromAllUserProfile();
+      const usersData = snapshot.docs.map((doc) => doc.data() as User);
+      setUsers(usersData);
+      console.log(usersData);
+    };
+
+    fetchUsers();
+  }, []);
 
   return (
     <>
       <TopBar />
       <Group p="xl" mx="xl" w="90%" position="apart" spacing="xl">
-        <TeacherCard
-          avatar="https://avatars.githubusercontent.com/u/50471205?v=4"
-          name="Luis"
-          university="Facens"
-          subject="Matematica"
-        />
-        <TeacherCard
-          avatar="https://avatars.githubusercontent.com/u/50471205?v=4"
-          name="Luis"
-          university="Facens"
-          subject="Matematica"
-        />
-        <TeacherCard
-          avatar="https://avatars.githubusercontent.com/u/50471205?v=4"
-          name="Luis"
-          university="Facens"
-          subject="Matematica"
-        />
-        <TeacherCard
-          avatar="https://avatars.githubusercontent.com/u/50471205?v=4"
-          name="Luis"
-          university="Facens"
-          subject="Matematica"
-        />
-        <TeacherCard
-          avatar="https://avatars.githubusercontent.com/u/50471205?v=4"
-          name="Luis"
-          university="Facens"
-          subject="Matematica"
-        />
+        {users.map((user) => (
+          <TeacherCard
+            key={user.uid} // Assuming each user has a unique "id" property
+            name={user.name}
+            university={user.university}
+            avatar=""
+            subject=""
+          />
+        ))}
       </Group>
     </>
   );
---
+++ src/types.tsx
@@ -0,0 +1,11 @@
+export interface User {
+  aboutYou: string;
+  city: string;
+  contact: string;
+  description: string;
+  money: number;
+  name: string;
+  subjects: string[];
+  uid: string;
+  university: string;
+}
