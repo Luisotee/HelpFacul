17476ed064a264402426338e28cb1f6e93bdb0ba
Luis Otavio <luisotaviomgr1@gmail.com>
2023-05-13 22:56:25 -0300

    User data now inserted when editing profile

--- src/components/user-profile-creation-page-helpers/fetch-user.tsx
+++ src/components/user-profile-creation-page-helpers/fetch-user.tsx
@@ -1,3 +1,4 @@
+import { getDataFromUserProfile } from "@/controller/firestore";
 import isLogged from "@/controller/isLogged";
 import { User } from "@/types";
 
@@ -7,5 +8,15 @@ export async function fetchUser(
 ) {
   const uid = await isLogged();
   setLoading(false);
-  setUser((prevUser) => ({ ...prevUser, uid: uid || "" }));
+  try {
+    if (uid) {
+      const userData = await getDataFromUserProfile(
+        "DCi7x9YzWNMFPu29u1DEfVXWkV52"
+      );
+      setUser((prevUser) => ({ ...prevUser, uid: uid, ...userData }));
+    }
+  } catch (error) {
+    console.error("Error fetching user data:", error);
+    // Handle the error appropriately, such as displaying an error message or redirecting the user
+  }
 }
--- src/controller/firestore.js
+++ src/controller/firestore.js
@@ -1,4 +1,13 @@
-import { getFirestore, collection, addDoc, getDocs } from "firebase/firestore";
+import {
+  getFirestore,
+  collection,
+  addDoc,
+  getDocs,
+  getDoc,
+  doc,
+  query,
+  where,
+} from "firebase/firestore";
 import app from "./firebase";
 
 // Function to insert user data into Firestore collection
@@ -24,4 +33,18 @@ export async function getDataFromAllUserProfile() {
   const snapshot = await getDocs(usersRef);
 
   return snapshot;
+}
+
+export async function getDataFromUserProfile(userId) {
+  const db = getFirestore(app);
+  const usersRef = collection(db, "users");
+  const q = query(usersRef, where("uid", "==", userId));
+  const querySnapshot = await getDocs(q);
+
+  if (!querySnapshot.empty) {
+    const userData = querySnapshot.docs[0].data();
+    return userData;
+  } else {
+    throw new Error("User not found");
+  }
 }
